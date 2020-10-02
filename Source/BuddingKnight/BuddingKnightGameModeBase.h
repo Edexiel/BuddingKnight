7b65ac57be20bd312af67f9915522d7c13c10a02
Guillaume NISI <g.nisi@student.isartdigital.com>
2020-10-02 13:37:16 +0200

    First commit

---
+++ Source/BuddingKnight/BuddingKnightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuddingKnightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUDDINGKNIGHT_API ABuddingKnightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
