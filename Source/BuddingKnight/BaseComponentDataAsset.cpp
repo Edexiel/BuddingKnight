5332050091273844ce3c8c12ce9fdbf8d7e0bcd8
Alex FIGUEIREDO <a.figueiredo@student.isartdigital.com>
2020-10-19 13:44:12 +0200

    Create a data asset for baseComponent

---
+++ Source/BuddingKnight/BaseComponentDataAsset.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseComponentDataAsset.h"
+
+float UBaseComponentDataAsset::GetHealth() const
+{
+    return Health;
+}
+
+float UBaseComponentDataAsset::GetMaxHealth() const
+{
+    return MaxHealth;
+}
