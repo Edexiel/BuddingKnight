d97df1edef4d751a7b2361064ca38a5676b72d11
Guillaume NISI <g.nisi@student.isartdigital.com>
2020-10-23 09:35:09 +0200

    push before merge

--- Source/BuddingKnight/AIC_EnemyCAC.cpp
+++ Source/BuddingKnight/AIC_EnemyCAC.cpp
@@ -2,6 +2,8 @@
 
 #include "PlayerCharacter.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 #include "Kismet/GameplayStatics.h"
 
 void AAIC_EnemyCAC::BeginPlay()
@@ -13,7 +15,9 @@ void AAIC_EnemyCAC::BeginPlay()
         RunBehaviorTree(BTAsset);
         Blackboard = GetBlackboardComponent();
     }
+    
     PlayerCharacter = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerPawn(this,0));
+    
     if (!Blackboard->GetValueAsObject("Player"))
         Blackboard->SetValueAsObject("Player",PlayerCharacter);
 
@@ -28,11 +32,15 @@ void AAIC_EnemyCAC::BeginPlay()
 
     if(!Blackboard->GetValueAsFloat("DistanceFromPlayer"))
         Blackboard->SetValueAsFloat("DistanceFromPlayer",GetPawn()->GetDistanceTo(PlayerCharacter));
+
+    UCharacterMovementComponent* MovementComponent= GetPawn()->GetController()->GetCharacter()->GetCharacterMovement();
+    MovementComponent->SetUpdateNavAgentWithOwnersCollisions(true);
+    MovementComponent->SetAvoidanceEnabled(true);
+
  
 }
 
 void AAIC_EnemyCAC::Tick(float DeltaTime)
 {
     Blackboard->SetValueAsFloat("DistanceFromPlayer",GetPawn()->GetDistanceTo(PlayerCharacter));
-
 }
